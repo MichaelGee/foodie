910ed3a304f0fe34792a0a46322a5c9c7df819fd
Michael <michaelgatesjr@gmail.com>
2020-04-09 09:40:59 +0100

    changed the width for the media quaries

--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,7 @@ import Logo from "./dish.svg";
 const Header = () => {
   return (
     <div className='header mb-8'>
-      <nav className='flex items-center align-middle justify-between justify-center flex-wrap bg-pink-800 p-3'>
+      <nav className='align-middle  bg-pink-800 p-3'>
         <div className='flex items-center text-white mr-6'>
           <img src={Logo} alt='food_logo' className='w-10 mr-1' />
           <span className='font-semibold text-3xl tracking-tight'>Foodiz</span>
